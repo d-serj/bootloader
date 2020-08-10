a3a68156a760ce820f7355504b520253098d1c32
Serj_D <serj.dub1na@gmail.com>
2020-08-10 22:41:46 +0300

    Add toolbox.h with ARRAY_SIZE macro

---
+++ source/toolbox.h
@@ -0,0 +1,16 @@
+/**
+ * @file toolbox.h
+ *
+ */
+
+#ifndef TOOLBOX_H_
+#define TOOLBOX_H_
+
+/**
+ * @brief Number of elements within an array
+ */
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+#endif
+
+#endif // TOOLBOX_H_
