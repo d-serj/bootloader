5a37ebfb8c421420f1f26c70c37263adf2b28c12
d-serj <serj.dub1na@gmail.com>
2020-10-18 21:07:58 +0300

    Add function to power on/off SIM800 module

---
+++ source/sim800/sim800.c
@@ -0,0 +1,57 @@
+/**
+ * @file sim800.c
+ */
+
+ #include <stdbool.h>
+
+#include <libopencm3/stm32/gpio.h>
+
+#include <pins.h>
+#include <delay.h>
+
+
+bool sim800_power_on(void)
+{
+  if (gpio_get(GSM_STATUS_GPIO_Port, GSM_STATUS_Pin))
+  {
+    return true;
+  }
+
+  gpio_set(SIM800_PWR_OFF_GPIO_Port, SIM800_PWR_OFF_Pin);
+  delay(10);
+  gpio_set(SIM800_PWR_KEY_GPIO_Port, SIM800_PWR_KEY_Pin);
+  delay(1100);
+  gpio_clear(SIM800_PWR_KEY_GPIO_Port, SIM800_PWR_KEY_Pin);
+
+  // Wait till SIM800 will be activated
+  delay(2500);
+
+  // Check SIM800 status pin
+  return !!gpio_get(GSM_STATUS_GPIO_Port, GSM_STATUS_Pin);
+}
+
+bool sim800_power_off(void)
+{
+  if (!gpio_get(GSM_STATUS_GPIO_Port, GSM_STATUS_Pin))
+  {
+    return true;
+  }
+
+  gpio_set(SIM800_PWR_KEY_GPIO_Port, SIM800_PWR_KEY_Pin);
+  delay(1100);
+  gpio_clear(SIM800_PWR_KEY_GPIO_Port, SIM800_PWR_KEY_Pin);
+  delay(10);
+
+  if ((!!gpio_get(GSM_STATUS_GPIO_Port, GSM_STATUS_Pin)) == false)
+  {
+    gpio_clear(SIM800_PWR_OFF_GPIO_Port, SIM800_PWR_OFF_Pin);
+  }
+  else
+  {
+    return false;
+  }
+
+  return true;
+}
+
+
