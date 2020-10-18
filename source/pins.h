23fc8afb913060126f42dc322647f29fedacc31e
d-serj <serj.dub1na@gmail.com>
2020-10-18 21:11:33 +0300

    Add pins.h with pins port and pins description

---
+++ source/pins.h
@@ -0,0 +1,31 @@
+/**
+ * @file pins.h
+ */
+
+#ifndef PINS_H_
+#define PINS_H_
+
+#include "libopencm3/stm32/gpio.h"
+
+#define CPU_STATUS_Pin           GPIO0
+#define CPU_STATUS_GPIO_Port     GPIOC
+
+#define SIGNAL_LED_Pin           GPIO5
+#define SIGNAL_LED_GPIO_Port     GPIOC
+
+#define GSM_STATUS_Pin           GPIO0
+#define GSM_STATUS_GPIO_Port     GPIOB
+
+#define SIM800_DTR_Pin           GPIO1
+#define SIM800_DTR_GPIO_Port     GPIOB
+
+#define SIM800_PWR_OFF_Pin       GPIO7
+#define SIM800_PWR_OFF_GPIO_Port GPIOC
+
+#define SIM800_PWR_KEY_Pin       GPIO8
+#define SIM800_PWR_KEY_GPIO_Port GPIOC
+
+#define SIM800_DCD_Pin           GPIO2
+#define SIM800_DCD_GPIO_Port     GPIOB
+
+#endif /* SOURCE_PINS_H_ */
