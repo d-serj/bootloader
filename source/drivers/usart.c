246666ac45d820947213ac91c2ef71e3502e066e
d-serj <serj.dub1na@gmail.com>
2020-10-25 23:33:09 +0200

    Update usart driver program module

--- source/drivers/usart.c
+++ source/drivers/usart.c
@@ -12,25 +12,27 @@
 #include "toolbox.h"
 #include "usart.h"
 
-#define USART_NUMBER   USART2
-#define USART_GPIO_TX  GPIO_USART2_TX
-#define USART_GPIO_RX  GPIO_USART2_RX
-#define USART_IRQ      NVIC_USART2_IRQ
+#define USART_NUMBER        USART2
+#define USART_GPIO_TX(NAME)  GPIO_##NAME##_TX
+#define USART_GPIO_RX(NAME)  GPIO_##NAME##_RX
+#define USART_IRQ(NAME)      NVIC_##NAME##_IRQ
 
-uint8_t u8_data[256]     = { 0 };
-uint8_t u8_data_received = 0;
+
+uint8_t u8_uart_data[256]        = { 0 };
+const uint32_t u32_data_arr_size = ARRAY_SIZE(u8_uart_data);
+uint8_t u8_uart_data_received    = 0;
 
 void usart_setup(void)
 {
-  nvic_enable_irq(USART_IRQ);
+  nvic_enable_irq(USART_IRQ(USART2));
 
   /* Setup GPIO pin GPIO_USART2_TX. */
   gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-                GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, USART_GPIO_TX);
+                GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, USART_GPIO_TX(USART2));
 
   /* Setup GPIO pin GPIO_USART2_RE_RX on GPIO port A for receive. */
   gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
-                GPIO_CNF_INPUT_FLOAT, USART_GPIO_RX);
+                GPIO_CNF_INPUT_FLOAT, USART_GPIO_RX(USART2));
 
   /* Setup UART parameters. */
   usart_set_baudrate(USART_NUMBER, 9600);
@@ -51,7 +53,7 @@ void usart_setup(void)
 
 void usart_deinit(void)
 {
-  nvic_disable_irq(USART_IRQ);
+  nvic_disable_irq(USART_IRQ(USART2));
   usart_disable(USART_NUMBER);
 }
 
@@ -64,6 +66,14 @@ void usart_send_string(const uint8_t *u8PL_str)
   }
 }
 
+void usart_clear_rx_buf(void)
+{
+  for (uint16_t u16L_i = 0; u16L_i < ARRAY_SIZE(u8_uart_data); ++u16L_i)
+  {
+    u8_uart_data[u16L_i] = 0;
+  }
+}
+
 #if (USART_NUMBER == USART2)
 void usart2_isr(void)
 {
@@ -73,10 +83,10 @@ void usart2_isr(void)
   if (((USART_CR1(USART_NUMBER) & USART_CR1_RXNEIE) != 0)
     && ((USART_SR(USART_NUMBER) & USART_SR_RXNE) != 0))
   {
-    u8_data[u16SL_idx] = usart_recv(USART_NUMBER);
+    u8_uart_data[u16SL_idx] = usart_recv(USART_NUMBER);
 
     /* Retrieve the data from the peripheral. */
-    if (u16SL_idx < ARRAY_SIZE(u8_data))
+    if (u16SL_idx < u32_data_arr_size)
     {
       ++u16SL_idx;
     }
@@ -88,24 +98,16 @@ void usart2_isr(void)
   else if (((USART_CR1(USART_NUMBER) & USART_CR1_IDLEIE) != 0)
     && ((USART_SR(USART_NUMBER) & USART_SR_IDLE) != 0))
   {
-    const uint16_t u16L_arr_size = ARRAY_SIZE(u8_data) - 1;
-    volatile uint32_t u32L_tmp   = 0;
+    volatile uint32_t u32L_tmp = 0;
 
     // Clear IDLE flag by reading status register first
     // And follow by reading data register
     u32L_tmp = USART_SR(USART_NUMBER);
     u32L_tmp = USART_DR(USART_NUMBER);
 
-    if (u16SL_idx < u16L_arr_size)
-    {
-      u8_data[u16SL_idx] = '\0';
-    }
-    else
-    {
-      u8_data[u16L_arr_size] = '\0';
-    }
+    u8_uart_data[u16SL_idx] = '\0';
 
-    u8_data_received = 1;
+    u8_uart_data_received = 1;
     u16SL_idx        = 0;
   }
 }
--- source/drivers/usart.h
+++ source/drivers/usart.h
@@ -9,10 +9,13 @@
 #include <stdint.h>
 
 /** RX buffer */
-extern uint8_t u8_data[256];
+extern uint8_t u8_uart_data[256];
+
+/** RX buffer size */
+extern const uint32_t u32_data_arr_size;
 
 /** Flag that indicates that data was received */
-extern uint8_t u8_data_received;
+extern uint8_t u8_uart_data_received;
 
 /**
  * @brief Initialize USART
@@ -30,6 +33,11 @@ void usart_deinit(void);
  * @warning String must have null terminator at the end
  */
 void usart_send_string(const uint8_t *u8PL_str);
+
+/**
+ * @brief Clear receive buffer
+ */
+void usart_clear_rx_buf(void);
 
 #endif // USART_H_
 
