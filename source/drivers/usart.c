7140909402afa22a15912289df4e9b622df3e618
Sergey Dubyna <sergey.dubyna@sigmaukraine.com>
2020-11-21 18:39:35 +0200

    Update usart module

--- source/drivers/usart.c
+++ source/drivers/usart.c
@@ -4,7 +4,10 @@
  */
 
 #include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
 
+#include "libopencm3/stm32/rcc.h"
 #include "libopencm3/stm32/usart.h"
 #include "libopencm3/stm32/gpio.h"
 #include "libopencm3/cm3/nvic.h"
@@ -12,81 +15,146 @@
 #include "toolbox.h"
 #include "usart.h"
 
-#define USART_NUMBER        USART2
-#define USART_GPIO_TX(NAME)  GPIO_##NAME##_TX
-#define USART_GPIO_RX(NAME)  GPIO_##NAME##_RX
-#define USART_IRQ(NAME)      NVIC_##NAME##_IRQ
+
+/** USART2 RX buffer */
+static uint8_t u8_usart2_data[UART_RX_BUFFER_SIZE];
+
+/** USART2 RX buffer */
+static uint8_t u8_uart4_data[UART_RX_BUFFER_SIZE];
+
+/** RX buffer size */
+const uint16_t u16_usart2_data_arr_size = ARRAY_SIZE(u8_usart2_data);
+const uint16_t u16_uart4_data_arr_size  = ARRAY_SIZE(u8_uart4_data);
+
+/** Flag that indicates that data was received */
+volatile uint8_t u8_usart2_data_received;
+volatile uint8_t u8_uart4_data_received;
 
 
-uint8_t u8_uart_data[256]        = { 0 };
-const uint32_t u32_data_arr_size = ARRAY_SIZE(u8_uart_data);
-uint8_t u8_uart_data_received    = 0;
-
-void usart_setup(void)
+void usart_setup(usart_instance_t *objPL_uart)
 {
-  nvic_enable_irq(USART_IRQ(USART2));
+  assert(objPL_uart != NULL);
+  const uart_num_t eL_uart_num = objPL_uart->e_instance;
+  assert((eL_uart_num == eUART2) || (eL_uart_num == eUART4));
 
-  /* Setup GPIO pin GPIO_USART2_TX. */
-  gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-                GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, USART_GPIO_TX(USART2));
+  if (eL_uart_num == eUART2)
+  {
+    rcc_periph_clock_enable(RCC_USART2);
 
-  /* Setup GPIO pin GPIO_USART2_RE_RX on GPIO port A for receive. */
-  gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
-                GPIO_CNF_INPUT_FLOAT, USART_GPIO_RX(USART2));
+    nvic_enable_irq(NVIC_USART2_IRQ);
+    
+    /* Setup GPIO pin GPIO_USART2_TX. */
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+        GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
+
+    /* Setup GPIO pin GPIO_USART2_RE_RX on GPIO port A for receive. */
+    gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
+        GPIO_CNF_INPUT_FLOAT, GPIO_USART2_RX);
+
+    objPL_uart->u8P_buffer        = u8_usart2_data;
+    objPL_uart->u16_buff_size     = u16_usart2_data_arr_size;
+    objPL_uart->u8P_data_received = &u8_usart2_data_received;
+  }
+  else if (eL_uart_num == eUART4)
+  {
+    rcc_periph_clock_enable(RCC_UART4);
+
+    nvic_enable_irq(NVIC_UART4_IRQ);
+
+        /* Setup GPIO pin GPIO_USART2_TX. */
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
+        GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_UART4_TX);
+
+    /* Setup GPIO pin GPIO_USART2_RE_RX on GPIO port A for receive. */
+    gpio_set_mode(GPIOC, GPIO_MODE_INPUT,
+        GPIO_CNF_INPUT_FLOAT, GPIO_UART4_RX);
+
+    objPL_uart->u8P_buffer        = u8_uart4_data;
+    objPL_uart->u16_buff_size     = u16_uart4_data_arr_size;
+    objPL_uart->u8P_data_received = &u8_uart4_data_received;
+  }
 
   /* Setup UART parameters. */
-  usart_set_baudrate(USART_NUMBER, 9600);
-  usart_set_databits(USART_NUMBER, 8);
-  usart_set_stopbits(USART_NUMBER, USART_STOPBITS_1);
-  usart_set_mode(USART_NUMBER, USART_MODE_TX_RX);
-  usart_set_parity(USART_NUMBER, USART_PARITY_NONE);
-  usart_set_flow_control(USART_NUMBER, USART_FLOWCONTROL_NONE);
+  usart_set_baudrate(eL_uart_num, 9600);
+  usart_set_databits(eL_uart_num, 8);
+  usart_set_stopbits(eL_uart_num, USART_STOPBITS_1);
+  usart_set_mode(eL_uart_num, USART_MODE_TX_RX);
+  usart_set_parity(eL_uart_num, USART_PARITY_NONE);
+  usart_set_flow_control(eL_uart_num, USART_FLOWCONTROL_NONE);
 
   /* Enable USART Idle line detection */
-  USART_CR1(USART_NUMBER) |= USART_CR1_IDLEIE;
+  USART_CR1(eL_uart_num) |= USART_CR1_IDLEIE;
   /* Enable USART Receive interrupt. */
-  USART_CR1(USART_NUMBER) |= USART_CR1_RXNEIE;
+  USART_CR1(eL_uart_num) |= USART_CR1_RXNEIE;
 
   /* Finally enable the USART. */
-  usart_enable(USART_NUMBER);
+  usart_enable(eL_uart_num);
 }
 
-void usart_deinit(void)
+void usart_deinit(usart_instance_t *objPL_uart)
 {
-  nvic_disable_irq(USART_IRQ(USART2));
-  usart_disable(USART_NUMBER);
-}
+  const uart_num_t eL_uart_num = objPL_uart->e_instance;
+  assert((eL_uart_num == eUART2) || (eL_uart_num == eUART4));
 
-void usart_send_string(const uint8_t *u8PL_str)
-{
-  while(*u8PL_str != '\0')
+  if (eL_uart_num == eUART2)
   {
-    usart_send_blocking(USART_NUMBER, (uint16_t)*u8PL_str);
-    ++u8PL_str;
+    nvic_disable_irq(NVIC_USART2_IRQ);
+  }
+  else if (eL_uart_num == eUART4)
+  {
+    nvic_disable_irq(NVIC_UART4_IRQ);
+  }
+
+  usart_disable(eL_uart_num);
+}
+
+void usart_send_string(const usart_instance_t *objPL_uart, const char *cPL_str)
+{
+  assert(objPL_uart != NULL);
+  assert((objPL_uart->e_instance == eUART2) || (objPL_uart->e_instance == eUART4));
+
+  while(*cPL_str != '\0')
+  {
+    usart_send_blocking(objPL_uart->e_instance, (uint16_t)*cPL_str);
+    ++cPL_str;
   }
 }
 
-void usart_clear_rx_buf(void)
+void usart_send_raw(const usart_instance_t *objPL_uart,
+  const uint8_t *u8PL_data, uint16_t u16L_data_size)
 {
-  for (uint16_t u16L_i = 0; u16L_i < ARRAY_SIZE(u8_uart_data); ++u16L_i)
+  assert(objPL_uart != NULL);
+  assert((objPL_uart->e_instance == eUART2) || (objPL_uart->e_instance == eUART4));
+
+  for (uint16_t u16L_i = 0; u16L_i < u16L_data_size; ++u16L_i)
   {
-    u8_uart_data[u16L_i] = 0;
+    usart_send_blocking(objPL_uart->e_instance, (uint16_t)u8PL_data[u16L_i]);
   }
 }
 
-#if (USART_NUMBER == USART2)
+void usart_clear_rx_buf(usart_instance_t *objPL_uart)
+{
+  assert(objPL_uart != NULL);
+  assert((objPL_uart->e_instance == eUART2) || (objPL_uart->e_instance == eUART4));
+
+  for (uint16_t u16L_i = 0; u16L_i < objPL_uart->u16_buff_size; ++u16L_i)
+  {
+    objPL_uart->u8P_buffer[u16L_i] = 0;
+  }
+}
+
 void usart2_isr(void)
 {
   static uint16_t u16SL_idx  = 0;
 
   /* Check if we were called because of RXNE. */
-  if (((USART_CR1(USART_NUMBER) & USART_CR1_RXNEIE) != 0)
-    && ((USART_SR(USART_NUMBER) & USART_SR_RXNE) != 0))
+  if (((USART2_CR1 & USART_CR1_RXNEIE) != 0)
+    && ((USART2_SR & USART_SR_RXNE) != 0))
   {
-    u8_uart_data[u16SL_idx] = usart_recv(USART_NUMBER);
+    u8_usart2_data[u16SL_idx] = usart_recv(USART2);
 
     /* Retrieve the data from the peripheral. */
-    if (u16SL_idx < u32_data_arr_size)
+    if (u16SL_idx < u16_usart2_data_arr_size)
     {
       ++u16SL_idx;
     }
@@ -95,20 +163,56 @@ void usart2_isr(void)
       u16SL_idx = 0;
     }
   }
-  else if (((USART_CR1(USART_NUMBER) & USART_CR1_IDLEIE) != 0)
-    && ((USART_SR(USART_NUMBER) & USART_SR_IDLE) != 0))
+  else if (((USART2_CR1 & USART_CR1_IDLEIE) != 0)
+    && ((USART2_SR & USART_SR_IDLE) != 0))
   {
     volatile uint32_t u32L_tmp = 0;
 
     // Clear IDLE flag by reading status register first
     // And follow by reading data register
-    u32L_tmp = USART_SR(USART_NUMBER);
-    u32L_tmp = USART_DR(USART_NUMBER);
+    u32L_tmp = USART2_SR;
+    u32L_tmp = USART2_DR;
 
-    u8_uart_data[u16SL_idx] = '\0';
+    u8_usart2_data[u16SL_idx] = '\0';
 
-    u8_uart_data_received = 1;
-    u16SL_idx        = 0;
+    u8_usart2_data_received = 1;
+    u16SL_idx               = 0;
   }
 }
-#endif
+
+void uart4_isr(void)
+{
+  static uint16_t u16SL_idx  = 0;
+
+  /* Check if we were called because of RXNE. */
+  if (((UART4_CR1 & USART_CR1_RXNEIE) != 0)
+    && ((UART4_SR & USART_SR_RXNE) != 0))
+  {
+    u8_uart4_data[u16SL_idx] = usart_recv(UART4);
+
+    /* Retrieve the data from the peripheral. */
+    if (u16SL_idx < u16_uart4_data_arr_size)
+    {
+      ++u16SL_idx;
+    }
+    else
+    {
+      u16SL_idx = 0;
+    }
+  }
+  else if (((UART4_CR1 & USART_CR1_IDLEIE) != 0)
+    && ((UART4_SR & USART_SR_IDLE) != 0))
+  {
+    volatile uint32_t u32L_tmp = 0;
+
+    // Clear IDLE flag by reading status register first
+    // And follow by reading data register
+    u32L_tmp = UART4_SR;
+    u32L_tmp = UART4_DR;
+
+    u8_uart4_data[u16SL_idx] = '\0';
+
+    u8_uart4_data_received = 1;
+    u16SL_idx              = 0;
+  }
+}
--- source/drivers/usart.h
+++ source/drivers/usart.h
@@ -8,36 +8,48 @@
 
 #include <stdint.h>
 
-/** RX buffer */
-extern uint8_t u8_uart_data[256];
+#include "libopencm3/stm32/usart.h"
 
-/** RX buffer size */
-extern const uint32_t u32_data_arr_size;
+#define UART_RX_BUFFER_SIZE  256u
 
-/** Flag that indicates that data was received */
-extern uint8_t u8_uart_data_received;
+enum USART_INSTANCES
+{
+  eUART2 = USART2,
+  eUART4 = UART4,
+};
+/** Type definition of UART number */
+typedef enum USART_INSTANCES uart_num_t;
+
+struct usart_instance
+{
+  uint8_t *u8P_buffer;
+  uint16_t u16_buff_size;
+  volatile uint8_t *u8P_data_received;
+  uart_num_t e_instance;
+};
+typedef struct usart_instance usart_instance_t;
 
 /**
  * @brief Initialize USART
  */
-void usart_setup(void);
+void usart_setup(usart_instance_t *objPL_uart);
 
 /**
  * @brief Deinitialize USART
  */
-void usart_deinit(void);
+void usart_deinit(usart_instance_t *objPL_uart);
 
 /**
  * @brief Send null terminated string via USART
  * @param u8PL_str .. pointer to the string
  * @warning String must have null terminator at the end
  */
-void usart_send_string(const uint8_t *u8PL_str);
+void usart_send_string(const usart_instance_t *objPL_uart, const char *cPL_str);
 
 /**
  * @brief Clear receive buffer
  */
-void usart_clear_rx_buf(void);
+void usart_clear_rx_buf(usart_instance_t *objPL_uart);
 
 #endif // USART_H_
 
