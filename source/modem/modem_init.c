5614eca5d3f76808a5d626677b1e8084a18235f7
d-serj <serj.dub1na@gmail.com>
2020-12-15 00:20:00 +0200

    Add source files for Modem module

---
+++ source/modem/modem_init.c
@@ -0,0 +1,6 @@
+/**
+ * @file modem_init.c
+ * @brief
+ */
+
+#include "modem_init.h"
---
+++ source/modem/modem_init.h
@@ -0,0 +1,11 @@
+/**
+ * @file modem_init.h
+ * @brief 
+ */
+
+#ifndef MODEM_INIT_H_
+#define MODEM_INIT_H_
+
+
+
+#endif // MODEM_INIT_H_
