70b95a1e71eb9d5b9f84733cac7c78436cc4d716
Sergey Dubyna <sergey.dubyna@sigmaukraine.com>
2021-01-22 13:53:37 +0200

    [DRIVERS] Update includes

--- source/drivers/usart.c
+++ source/drivers/usart.c
@@ -7,12 +7,13 @@
 #include <stddef.h>
 #include <assert.h>
 
-#include "libopencm3/stm32/rcc.h"
-#include "libopencm3/stm32/usart.h"
-#include "libopencm3/stm32/gpio.h"
-#include "libopencm3/cm3/nvic.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
 
-#include "toolbox.h"
+#include <utilities/toolbox.h>
+
 #include "usart.h"
 
 
