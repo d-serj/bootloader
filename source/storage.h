b8742fbe4956d16d91100475dbad745cbf022f14
d-serj <sp11kenny@gmail.com>
2021-02-15 22:28:30 +0200

    Add functions to init and deinit storage

--- source/storage.h
+++ source/storage.h
@@ -8,6 +8,17 @@
 
 #include <stdint.h>
 
+
+/**
+ * @brief Initialize storage
+ */
+void storage_init(void);
+
+/**
+ * @brief Deinitialize storage
+ */
+void storage_deinit(void);
+
 /**
  * @brief Get the file chunk from the storage
  * 
@@ -21,6 +32,13 @@ uint32_t storage_get_chunk(const char *cPL_file_name,
                            uint32_t u32L_chunk_addr,
                            uint8_t *u8PL_chunk_buf,
                            uint32_t u32L_chunk_size);
+
+/**
+ * @brief Get file size
+ * 
+ * @param cPL_file_name .. filename to give the size of
+ * @return uint32_t size of the file, 0 if file was not found
+ */
 uint32_t storage_get_file_size(const char *cPL_file_name);
 
 #endif // STORAGE_H_
