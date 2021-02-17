ad8c5ec4570e6dc04c1d05ca3446f33a714a9c72
d-serj <sp11kenny@gmail.com>
2021-02-17 19:33:28 +0200

    Add stubs for some libopencm3 drivers

---
+++ test/src/stubs/delay.h
@@ -0,0 +1,13 @@
+/**
+ * @file delay.h
+ * @brief stubs for delay.c/h
+ */
+
+#ifndef STUB_DELAY_H_
+#define STUB_DELAY_H_
+
+#include <stdint.h>
+
+void delay(uint64_t u64L_duration);
+
+#endif // STUB_DELAY_H_
---
+++ test/src/stubs/libopencm3/stm32/usart.c
@@ -0,0 +1,127 @@
+
+#include "usart.h"
+
+void usart_set_baudrate(uint32_t usart, uint32_t baud)
+{
+
+}
+
+void usart_set_databits(uint32_t usart, uint32_t bits)
+{
+
+}
+
+void usart_set_stopbits(uint32_t usart, uint32_t stopbits)
+{
+
+}
+
+void usart_set_parity(uint32_t usart, uint32_t parity)
+{
+
+}
+
+void usart_set_mode(uint32_t usart, uint32_t mode)
+{
+
+}
+
+void usart_set_flow_control(uint32_t usart, uint32_t flowcontrol)
+{
+
+}
+
+void usart_enable(uint32_t usart)
+{
+
+}
+
+void usart_disable(uint32_t usart)
+{
+
+}
+
+void usart_send(uint32_t usart, uint16_t data)
+{
+
+}
+
+uint16_t usart_recv(uint32_t usart)
+{
+
+}
+
+void usart_wait_send_ready(uint32_t usart)
+{
+
+}
+
+void usart_wait_recv_ready(uint32_t usart)
+{
+
+}
+
+void usart_send_blocking(uint32_t usart, uint16_t data)
+{
+
+}
+
+uint16_t usart_recv_blocking(uint32_t usart)
+{
+
+}
+
+void usart_enable_rx_dma(uint32_t usart)
+{
+
+}
+
+void usart_disable_rx_dma(uint32_t usart)
+{
+
+}
+
+void usart_enable_tx_dma(uint32_t usart)
+{
+
+}
+
+void usart_disable_tx_dma(uint32_t usart)
+{
+
+}
+
+void usart_enable_rx_interrupt(uint32_t usart)
+{
+
+}
+
+void usart_disable_rx_interrupt(uint32_t usart)
+{
+
+}
+
+void usart_enable_tx_interrupt(uint32_t usart)
+{
+
+}
+
+void usart_disable_tx_interrupt(uint32_t usart)
+{
+
+}
+
+void usart_enable_error_interrupt(uint32_t usart)
+{
+
+}
+
+void usart_disable_error_interrupt(uint32_t usart)
+{
+
+}
+
+bool usart_get_flag(uint32_t usart, uint32_t flag)
+{
+
+}
