f885c5faa83a717efcf0b8c3455555125b1a06e1
d-serj <serj.dub1na@gmail.com>
2021-02-18 23:55:22 +0200

    Update test_image.c

--- test/src/tests/test_image.c
+++ test/src/tests/test_image.c
@@ -12,27 +12,39 @@
 
 #include <storage.h>
 #include <image.h>
+#include <utilities/toolbox.h>
 
 #include "test_image.h"
 
 DEFINE_FFF_GLOBALS;
 
 FAKE_VOID_FUNC(usart_send_blocking, uint32_t, uint16_t);
-FAKE_VALUE_FUNC(uint16_t, usart_recv, uint32_t);
+
+char cPL_buff[256]       = { 0 };
+static uint16_t s16S_idx = 0;
+
+uint16_t usart_recv(uint32_t usart)
+{
+  return (uint16_t)cPL_buff[s16S_idx++];
+}
 
 void setUp(void)
 {
   storage_init();
+  snprintf(cPL_buff, ARRAY_SIZE(cPL_buff), "AT+FSREAD=%s,%d,%d,%d\r\n", 
+    "firmware.bin", 0, sizeof(image_hdr_t), 0);
 }
 
 void tearDown(void)
 {
-  
+  s16S_idx = 0;
 }
 
 void test_ImageImageSize(void)
 {
+  RESET_FAKE(usart_send_blocking);
   const image_hdr_t objL_hdr = image_header_get();
+  TEST_ASSERT_EQUAL(strlen(cPL_buff), usart_send_blocking_fake.call_count);
   TEST_ASSERT_EQUAL(image_get_size(), objL_hdr.u32_data_size);
 }
 
@@ -45,5 +57,5 @@ void test_ImageHeaderCheck(void)
 void test_ImageValidateTest(void)
 {
   const image_hdr_t objL_hdr = image_header_get();
-  //TEST_ASSERT_TRUE(image_validate(&objL_hdr));
+  TEST_ASSERT_TRUE(image_validate(&objL_hdr));
 }
