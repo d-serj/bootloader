cc6fbe85dd003c7b5935822ed441f9646171075d
d-serj <serj.dub1na@gmail.com>
2021-02-18 23:53:18 +0200

    Update unit tests for storage.c

--- test/src/tests/test_storage.c
+++ test/src/tests/test_storage.c
@@ -22,23 +22,50 @@ DEFINE_FFF_GLOBALS;
 FAKE_VOID_FUNC(usart_send_blocking, uint32_t, uint16_t);
 FAKE_VALUE_FUNC(uint16_t, usart_recv, uint32_t);
 
+// Helpers
+
+static void usart_accumulate_data(const uint8_t *u8PL_data, size_t sL_size)
+{
+  for (size_t i = 0; i < sL_size; ++i)
+  {
+    usart_recv_fake.return_val = (uint16_t)u8PL_data[i];
+    // Call interrupt handler
+    usart2_isr();
+  }
+}
+
+// Tests
+
 void test_storage_compare_echo(void)
 {
   storage_init();
+  
+  RESET_FAKE(usart_send_blocking);
+  RESET_FAKE(usart_recv);
 
   const char *cPL_file_name = "test.txt";
   char cPL_buff[128]        = { 0 };
   
   snprintf(cPL_buff, ARRAY_SIZE(cPL_buff), "AT+FSREAD=%s,%d,%ld,%ld\r\n",
     cPL_file_name, 0, 56, 0);
-  
-  char cPL_rec_buff[256] = { 0 };
 
-  for (size_t i = 0; i < ARRAY_SIZE(cPL_buff); ++i)
-  {
-    usart_recv_fake.return_val = cPL_buff[i];
-    usart2_isr();
-  }
+  usart_accumulate_data((const uint8_t*)cPL_buff, ARRAY_SIZE(cPL_buff));
   
   TEST_ASSERT_TRUE(storage_compare_echo(cPL_buff, strlen(cPL_buff)));
+}
+
+void test_storage_get_file_size(void)
+{
+  RESET_FAKE(usart_send_blocking);
+  RESET_FAKE(usart_recv);
+
+  char cPL_buff[128] = { 0 };
+  const char *cPL_cmd = "AT+FSFLSIZE=firmware.bin\r\n20\r\n";
+  
+  strncpy(cPL_buff, cPL_cmd, strlen(cPL_cmd));
+
+  usart_accumulate_data((const uint8_t*)cPL_buff, ARRAY_SIZE(cPL_buff));
+
+  TEST_ASSERT_EQUAL_UINT32(20, storage_get_file_size("firmware.bin"));
+  TEST_ASSERT_EQUAL_CHAR('A', usart_send_blocking_fake.arg0_history[0]);
 }
