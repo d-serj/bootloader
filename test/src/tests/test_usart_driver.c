5281011e354fff7d0f50277b2e5a2046283dd7d5
d-serj <sp11kenny@gmail.com>
2021-02-18 18:26:40 +0200

    Update usart_driver unit tests

--- test/src/tests/test_usart_driver.c
+++ test/src/tests/test_usart_driver.c
@@ -30,6 +30,35 @@ void tearDown(void)
   
 }
 
+// Helpers
+
+static void usart_accumulate_data(const uint8_t *u8PL_data, size_t sL_size)
+{
+  for (size_t i = 0; i < sL_size; ++i)
+  {
+    usart_recv_fake.return_val = (uint16_t)u8PL_data[i];
+    // Call interrupt handler
+    usart2_isr();
+  }
+}
+
+static void usart_receive_accumulated_data(uint8_t *u8PL_data, size_t sL_size)
+{
+  uint8_t u8L_byte = 0;
+
+  for (size_t i = 0; i < sL_size; ++i)
+  {
+    if (usart_get_byte(&objS_uart2, &u8L_byte, 1) == 0)
+    {
+      return;
+    }
+    
+    u8PL_data[i] = u8L_byte;
+  }
+}
+
+// Tests
+
 void test_usart_send_string(void)
 {
   const char *cPL_string_to_send         = "Hello world!";
@@ -51,36 +80,27 @@ void test_usart_get_byte(void)
   usart_recv_fake.return_val = 'H';
   usart2_isr();
   char cL_rec = '\0';
-  usart_get_byte(&objS_uart2, (uint8_t*)&cL_rec, 10);
+  TEST_ASSERT_TRUE(usart_get_byte(&objS_uart2, (uint8_t*)&cL_rec, 10));
   TEST_ASSERT_EQUAL_CHAR('H', cL_rec);
 
   // Receive 'e' from ISR
   usart_recv_fake.return_val = 'e';
   usart2_isr();
-  usart_get_byte(&objS_uart2, (uint8_t*)&cL_rec, 10);
+  TEST_ASSERT_TRUE(usart_get_byte(&objS_uart2, (uint8_t*)&cL_rec, 10));
   TEST_ASSERT_EQUAL_CHAR('e', cL_rec);
 
   // Timeout
   TEST_ASSERT_EQUAL_UINT8(0, usart_get_byte(&objS_uart2, (uint8_t*)&cL_rec, 1));
-  
+  // The function should have been called two times
   TEST_ASSERT_EQUAL_UINT8(2, usart_recv_fake.call_count);
 
   // Lets accumulate some data
   const uint8_t u8PL_data[] = { 0xAD, 0xDA, 0xFF, 0x01 };
-  for (size_t i = 0; i < ARRAY_SIZE(u8PL_data); ++i)
-  {
-    usart_recv_fake.return_val = (uint16_t)u8PL_data[i];
-    usart2_isr();
-  }
+  usart_accumulate_data(u8PL_data, ARRAY_SIZE(u8PL_data));
 
   // Receive accumulated data
   uint8_t u8PL_buff[50] = { 0 };
-  for (size_t i = 0; i < ARRAY_SIZE(u8PL_data); ++i)
-  {
-    uint8_t u8L_byte = 0;
-    TEST_ASSERT(usart_get_byte(&objS_uart2, &u8L_byte, 1));
-    u8PL_buff[i] = u8L_byte;
-  }
+  usart_receive_accumulated_data(u8PL_buff, ARRAY_SIZE(u8PL_buff));
 
   TEST_ASSERT_EQUAL_UINT8_ARRAY(u8PL_data, u8PL_buff, ARRAY_SIZE(u8PL_data));
 }
@@ -88,14 +108,11 @@ void test_usart_get_byte(void)
 void test_usart_flush(void)
 {
   const uint8_t u8PL_data[] = { 0xDE, 0xF5, 0x50, 0x11 };
-  for (size_t i = 0; i < ARRAY_SIZE(u8PL_data); ++i)
-  {
-    usart_recv_fake.return_val = (uint16_t)u8PL_data[i];
-    usart2_isr();
-  }
-
-  usart_flush(&objS_uart2);
-
+  usart_accumulate_data(u8PL_data, ARRAY_SIZE(u8PL_data));
+  
   uint8_t u8L_byte = 0;
+
+  TEST_ASSERT_TRUE(usart_get_byte(&objS_uart2, &u8L_byte, 1));
+  usart_flush(&objS_uart2);
   TEST_ASSERT_FALSE(usart_get_byte(&objS_uart2, &u8L_byte, 1));
 }
