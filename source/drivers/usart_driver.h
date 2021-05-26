2f45189996546546e119d7653ac579adcd348be6
d-serj <serj.dub1na@gmail.com>
2021-05-26 23:27:36 +0300

    Make usart_send_raw public

--- source/drivers/usart_driver.h
+++ source/drivers/usart_driver.h
@@ -61,5 +61,15 @@ void usart_flush(usart_instance_t *objPL_uart);
  */
 uint8_t usart_get_byte(usart_instance_t *objPL_uart, uint8_t *u8PL_byte, uint8_t u8L_timeout);
 
+/**
+ * @brief Send raw data via UART
+ * 
+ * @param objPL_uart .. pointer to USART instance
+ * @param u8PL_data .. pointer to the data
+ * @param u16L_data_size .. data size
+ */
+void usart_send_raw(const usart_instance_t *objPL_uart,
+  const uint8_t *u8PL_data, uint16_t u16L_data_size);
+
 #endif // USART_DRIVER_H_
 
