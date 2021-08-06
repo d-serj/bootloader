09b92c1a05b6b938cdf61495613c341238ddff2c
d-serj <serj.dub1na@gmail.com>
2021-08-06 19:37:48 +0300

    Update delay.c

--- source/delay.c
+++ source/delay.c
@@ -15,7 +15,7 @@ void systick_setup(void)
   // Set the systick clock source to our main clock
   systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
   // Clear the Current Value Register so that we start at 0
-  STK_CVR = 0;
+  systick_clear();
   // In order to trigger an interrupt every millisecond, we can set the reload
   // value to be the speed of the processor / 1000 - 1
   systick_set_reload(rcc_ahb_frequency / 1000 - 1);
