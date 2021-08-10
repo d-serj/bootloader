e29b28689e68709f91de7c7dec468bc861098884
d-serj <sp11kenny@gmail.com>
2021-08-10 18:30:10 +0300

    Update bkp module

--- source/drivers/bkp.c
+++ source/drivers/bkp.c
@@ -6,11 +6,32 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include <libopencm3/stm32/f1/pwr.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/pwr.h>
+#include <libopencm3/stm32/f1/bkp.h>
+
+#include <system/assert.h>
+
+void bkp_init(void)
+{
+  rcc_periph_clock_enable(RCC_PWR);
+  rcc_periph_clock_enable(RCC_BKP);
+}
+
+void bkp_deinit(void)
+{
+  rcc_periph_clock_disable(RCC_PWR);
+  rcc_periph_clock_disable(RCC_BKP);
+}
 
 void bkp_write(uint32_t u32L_bkp_reg, uint16_t u16L_value)
 {
   pwr_disable_backup_domain_write_protect();
   u32L_bkp_reg = u16L_value;
   pwr_enable_backup_domain_write_protect();
+}
+
+uint16_t bkp_read(uint32_t u32L_bkp_reg)
+{
+  return *(uint16_t*)u32L_bkp_reg;
 }
--- source/drivers/bkp.h
+++ source/drivers/bkp.h
@@ -3,6 +3,35 @@
  * @brief Backup registers
  */
 
+#ifndef BKP_H_
+#define BKP_H_
+
 #include <stdint.h>
 
+/**
+ * @brief Initialize backup registers
+ */
+void bkp_init(void);
+
+/**
+ * @brief Deinitialize backup registers
+ * 
+ */
+void bkp_deinit(void);
+
+/**
+ * @brief Write data to the given backup register
+ * 
+ * @param u32L_bkp_reg .. backup register BKP_DR1 ... BKP_DR42
+ * @param u16L_value .. the value to be written to the backup register
+ */
 void bkp_write(uint32_t u32L_bkp_reg, uint16_t u16L_value);
+
+/**
+ * @brief Read backup register
+ * @param u32L_bkp_reg .. backup register BKP_DR1 ... BKP_DR42
+ * @return uint16_t the value that was read from the register
+ */
+uint16_t bkp_read(uint32_t u32L_bkp_reg);
+
+#endif // BKP_H_
